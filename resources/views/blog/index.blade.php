7b537153850a5d34280f73d905998ad851cf1c09
lialia.sakhno <lintume@gmail.com>
2024-12-23 00:07:54 +0200

    add trans

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,18 +1,18 @@
 <x-app-layout>
 
-    @section('title', __('Блог - користні статті про харчування і здоровʼя'))
+    @section('title', __('Blog - useful articles about nutrition and health'))
 
     @section('meta')
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description"
-              content="{{ __('Корисні статті про здорове харчування, схуднення та фітнес. Дізнайтеся, як правильно рахувати калорії, які продукти вибирати для схуднення та як додаток Калорайз допоможе вам досягти мети.') }}">
-        <meta name="keywords" content="{{ __('статті про харчування, схуднення, здоровʼя, фітнес') }}">
+              content="{{ __('Useful articles about healthy eating, weight loss, and fitness. Learn how to count calories correctly, which foods to choose for weight loss, and how the Calorize app can help you reach your goals.') }}">
+        <meta name="keywords" content="{{ __('Articles about nutrition, weight loss, health, fitness') }}">
     @endsection
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <h1 class="text-2xl font-bold">{{ __('Корисні статті про харчування та здоровʼя') }}
+            <h1 class="text-2xl font-bold">{{ __('Useful articles about nutrition and health') }}
         </h2>
     </x-slot>
 
@@ -22,7 +22,7 @@
                 <ul class="space-y-4">
                     <li class="p-4 bg-gray-100 rounded-lg shadow-md hover:bg-gray-200 transition duration-300">
                         <a href="{{ route('blog-1') }}" class="text-blue-500 underline text-lg font-semibold hover:text-blue-700">
-                            {{ __('Як правильно рахувати калорії для схуднення — практичний гід') }}
+                            Як правильно рахувати калорії для схуднення — практичний гід
                         </a>
                     </li>
                 </ul>
