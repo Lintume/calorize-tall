c0075adb7c8ca591bcfba791f80e5cb23adecf66
lialia.sakhno <lintume@gmail.com>
2024-12-23 20:48:02 +0200

    fix indexation in google, pagination

--- resources/views/livewire/product-index.blade.php
+++ resources/views/livewire/product-index.blade.php
@@ -32,6 +32,16 @@
     </x-slot>
 
     @section('meta')
+        @if ($products->onFirstPage())
+            <link rel="next" href="{{ $products->nextPageUrl() }}">
+        @elseif ($products->hasMorePages())
+            <link rel="prev" href="{{ $products->previousPageUrl() }}">
+            <link rel="next" href="{{ $products->nextPageUrl() }}">
+        @else
+            <link rel="prev" href="{{ $products->previousPageUrl() }}">
+        @endif
+        <link rel="canonical" href="{{ url()->current() }}">
+
         <meta name="description"
               content="{{ __('List of products including their nutritional information such as proteins, fats, carbohydrates, and calories.') }}">
         <meta name="keywords" content="{{ __('Products, Nutrition, Proteins, Fats, Carbohydrates, Calories') }}">
@@ -41,46 +51,46 @@
         <meta property="og:type" content="website">
         <meta property="og:url" content="{{ url()->current() }}">
 
-            <script type="application/ld+json">
-                {!! json_encode([
-                    "@context" => "https://schema.org",
-                    "@type" => "ItemList",
-                    "name" => __("Product List"),
-                    "description" => __("List of products including nutritional information such as calories, proteins, fats, and carbohydrates."),
-                    "url" => url()->current(),
-                    "itemListElement" => $products->map(function ($product, $index) {
-                        return [
-                            "@type" => "ListItem",
-                            "position" => $index + 1,
-                            "url" => route('product.show', $product->id),
-                            "name" => $product->title,
-                            "additionalProperty" => [
-                                [
-                                    "@type" => "PropertyValue",
-                                    "name" => "Calories",
-                                    "value" => "{$product->calories} kcal",
-                                ],
-                                [
-                                    "@type" => "PropertyValue",
-                                    "name" => "Proteins",
-                                    "value" => "{$product->proteins} g",
-                                ],
-                                [
-                                    "@type" => "PropertyValue",
-                                    "name" => "Fats",
-                                    "value" => "{$product->fats} g",
-                                ],
-                                [
-                                    "@type" => "PropertyValue",
-                                    "name" => "Carbohydrates",
-                                    "value" => "{$product->carbohydrates} g",
-                                ],
+        <script type="application/ld+json">
+            {!! json_encode([
+                "@context" => "https://schema.org",
+                "@type" => "ItemList",
+                "name" => __("Product List"),
+                "description" => __("List of products including nutritional information such as calories, proteins, fats, and carbohydrates."),
+                "url" => url()->current(),
+                "itemListElement" => $products->map(function ($product, $index) {
+                    return [
+                        "@type" => "ListItem",
+                        "position" => $index + 1,
+                        "url" => route('product.show', $product->id),
+                        "name" => $product->title,
+                        "additionalProperty" => [
+                            [
+                                "@type" => "PropertyValue",
+                                "name" => "Calories",
+                                "value" => "{$product->calories} kcal",
                             ],
-                        ];
-                    }),
-                ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) !!}
-            </script>
-        @endsection
+                            [
+                                "@type" => "PropertyValue",
+                                "name" => "Proteins",
+                                "value" => "{$product->proteins} g",
+                            ],
+                            [
+                                "@type" => "PropertyValue",
+                                "name" => "Fats",
+                                "value" => "{$product->fats} g",
+                            ],
+                            [
+                                "@type" => "PropertyValue",
+                                "name" => "Carbohydrates",
+                                "value" => "{$product->carbohydrates} g",
+                            ],
+                        ],
+                    ];
+                }),
+            ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) !!}
+        </script>
+    @endsection
 
     <div class="flex flex-col shadow justify-between rounded-lg mt-5 bg-white mb-1">
         <div class="p-5 md:p-10">
@@ -146,7 +156,44 @@
                 </div>
                 <!-- Pagination -->
                 <div class="mt-4">
-                    {{ $products->links() }}
+                    <nav role="navigation" aria-label="Pagination Navigation" class="flex justify-between">
+            <span>
+                {{-- Previous Page Link --}}
+                @if ($products->onFirstPage())
+                    <span
+                        class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md dark:text-gray-600 dark:bg-gray-800 dark:border-gray-600">
+                        {!! __('pagination.previous') !!}
+                    </span>
+                @else
+                    <a href="{{ $products->previousPageUrl() }}">
+                    <button type="button" dusk="previousPage"
+                            wire:loading.attr="disabled"
+                            class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-blue-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-300 dark:focus:border-blue-700 dark:active:bg-gray-700 dark:active:text-gray-300">
+                                {!! __('pagination.previous') !!}
+                        </button>
+                    </a>
+                @endif
+            </span>
+
+                        <span>
+                {{-- Next Page Link --}}
+                            @if ($products->hasMorePages())
+                                <a href="{{ $products->nextPageUrl() }}">
+                                    <button type="button" dusk="nextPage"
+                                            wire:loading.attr="disabled"
+                                            class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-blue-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-300 dark:focus:border-blue-700 dark:active:bg-gray-700 dark:active:text-gray-300">
+                                {!! __('pagination.next') !!}
+                        </button>
+                                    </a>
+                            @else
+                                <span
+                                    class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md">
+                        {!! __('pagination.next') !!}
+                    </span>
+                            @endif
+            </span>
+                    </nav>
+                    {{--                    {{ $products->links() }}--}}
                 </div>
             @endif
         </div>
