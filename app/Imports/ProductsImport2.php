2f7e0da223c0d523235ea16f44c3978356e9ebfa
lialia.sakhno <lintume@gmail.com>
2024-12-23 22:53:49 +0200

    add mode products

---
+++ app/Imports/ProductsImport2.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class ProductsImport2 implements ToModel, WithHeadingRow, WithChunkReading
+{
+    public function model(array $row)
+    {
+        return new Product([
+            'title' => $row['title'],
+            'proteins' => $row['proteins'],
+            'fats' => $row['fats'],
+            'carbohydrates' => $row['carbohydrates'],
+            'calories' => $row['calories'],
+        ]);
+    }
+
+    public function chunkSize(): int
+    {
+        return 100; // Adjust the chunk size as needed
+    }
+}
