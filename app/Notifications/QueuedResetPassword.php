1d8a8332a3d32da42dc50e79720e894c64f4741b
lialia.sakhno <lintume@gmail.com>
2026-01-14 14:17:58 +0200

    Add mail representation to QueuedResetPassword notification
    - Implemented the toMail method to build the email notification for password resets, enhancing user communication during the password recovery process.
    - The method includes a subject, multiple lines of text, and an action button linking to the reset URL, improving the overall user experience.

--- app/Notifications/QueuedResetPassword.php
+++ app/Notifications/QueuedResetPassword.php
@@ -5,8 +5,27 @@ namespace App\Notifications;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class QueuedResetPassword extends ResetPassword implements ShouldQueue
 {
     use Queueable;
+
+    /**
+     * Build the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = $this->resetUrl($notifiable);
+
+        return (new MailMessage)
+            ->subject(__('passwords.reset_notification_subject'))
+            ->line(__('passwords.reset_notification_line1'))
+            ->action(__('passwords.reset_notification_action'), $url)
+            ->line(__('passwords.reset_notification_line2', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+            ->line(__('passwords.reset_notification_line3'));
+    }
 }
---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'reset' => 'Your password has been reset.',
+    'sent' => 'We have emailed your password reset link.',
+    'throttled' => 'Please wait before retrying.',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that email address.",
+
+    // Reset Password Notification
+    'reset_notification_subject' => 'Reset Password Notification',
+    'reset_notification_line1' => 'You are receiving this email because we received a password reset request for your account.',
+    'reset_notification_action' => 'Reset Password',
+    'reset_notification_line2' => 'This password reset link will expire in :count minutes.',
+    'reset_notification_line3' => 'If you did not request a password reset, no further action is required.',
+];
